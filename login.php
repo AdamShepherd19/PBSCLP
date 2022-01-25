0a681333dfd9573163df1545db9ecca500007099
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-01-25 10:54:52 +0000

    added connection error handling

--- login.php
+++ login.php
@@ -2,9 +2,12 @@
     if(isset($_POST['login'])) {
         $connection = new mysqli('localhost', 'pbsclp', '', 'pbsclp_pbsclp');
 
-        $email = $connection->$mysqli->real_escape_string($_POST['emailPHP']);
-        $password = $connection->$mysqli->real_escape_string($_POST['passwordPHP']);
+        $email = $connection->mysqli_real_escape_string($_POST['emailPHP']);
+        $password = $connection->mysqli_real_escape_string($_POST['passwordPHP']);
 
+        if ($conn->connect_error) {
+            exit("Connection failed: " . $conn->connect_error);
+        }
         exit($email . ' + ' . $password);
         // $data = $connection->query("SELECT id FROM users WHERE email='$email' AND password='$password'");
 
