e91478b0cf627e035d5ed27bf4625894bdc6ebae
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-01-27 16:03:49 +0000

    changed to exit again

--- landing.php
+++ landing.php
@@ -24,9 +24,9 @@
 
     if ($result->num_rows > 0) {
         // output data of each row
-        while($row = mysqli_fetch_assoc($result)) {
+        while($row = $result->fetch_assoc()) {
             // echo "- Title: " . $row["title"]. "<br>- Content: " . $row["content"]. "<br>- Author" . $row["author"]. "<br><br>";
-            $data.append($row);
+            $data[] = $row;
         }
     } else {
         echo "0 results";
