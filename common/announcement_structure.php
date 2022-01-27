412b6d2a11af1e761f8674d3b18349234ea8917e
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-01-27 14:56:51 +0000

    Added an announcement structure file

---
+++ common/announcement_structure.php
@@ -0,0 +1,11 @@
+<div class="card">
+    <div class="card-header">
+        <span class="announcement-header">Announcement 4 </span>
+        <div class="card-body">
+            <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer class="blockquote-footer">Firstname Surname</footer>
+            </blockquote>
+        </div>
+    </div>
+</div>
