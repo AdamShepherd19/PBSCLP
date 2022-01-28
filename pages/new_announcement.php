e77de9985c10c1cb7ea3bff94cb4955c51333481
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-01-28 21:13:57 +0000

    fixed query syntax

--- pages/new_announcement.php
+++ pages/new_announcement.php
@@ -24,7 +24,7 @@
 
         //query db for user login details provided
         // $query = "INSERT INTO `announcements` (`announcement_id`, `title`, `content`, `author`) VALUES (NULL, `" . $title . "`, `" . $content . "`, `" . $author . "`);";
-        $query = "INSERT INTO announcements (title, content, author) VALUES ('" . $title . "`, `" . $content . "`, `" . $author . "');";
+        $query = "INSERT INTO announcements (title, content, author) VALUES ('" . $title . "', '" . $content . "', '" . $author . "');";
         //check if login details provided match a user profile in the db
         if ($connection->query($query) === TRUE) {
             exit('success');
