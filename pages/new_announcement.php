8e21e69e81bc65cc5d7c6e2d0330144d3dfebd13
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-01-28 20:54:34 +0000

    hopefully fixed query

--- pages/new_announcement.php
+++ pages/new_announcement.php
@@ -29,7 +29,7 @@
         
         //query db for user login details provided
         // $query = "INSERT INTO `announcements` (`announcement_id`, `title`, `content`, `author`) VALUES (NULL, `" . $title . "`, `" . $content . "`, `" . $author . "`);";
-        $query = "INSERT INTO `announcements` (`announcement_id`, `title`, `content`, `author`) VALUES (NULL, `testTitle`, `testContent`, `testAuthor`);";
+        $query = "INSERT INTO announcements (title, content, author) VALUES ('testTitle2', 'testContent2', 'testAuthor2');";
         //check if login details provided match a user profile in the db
         if ($connection->query(query) === TRUE) {
             echo "test3";
