cb815116a6dd9d5a115a3249a85bd3cc42e6fab4
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-01-31 10:46:52 +0000

    removed old no announcements code

--- pages/landing.php
+++ pages/landing.php
@@ -104,8 +104,6 @@
                     dataType: 'JSON',
                     success: function(response) {
                         if (response.includes("*warning_no_announcements_found*")) {
-                            $(".left-space").after("<h3> There are no announcements yet! </h3>");
-                            // $("#announcement-wrapper").html("<h3> There are no announcements yet! </h3>");
                             var announcement = "<div class='card'>" +
                                 "<div class='card-header'>" +
                                     "<span class='announcement-header'> There are no announcements! </span></div>" +
