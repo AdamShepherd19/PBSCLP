e8785f08643fea3b028b75329ac40d78e20c93b0
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-01-31 13:41:38 +0000

    simpler query test

--- scripts/update_profile.php
+++ scripts/update_profile.php
@@ -24,7 +24,8 @@
         $lastname = $pieces[1];
 
         // query database and insert the new announcement into the announcements table
-        $query = "UPDATE users SET firstname='" . $firstname. "', lastname='" . $lastname . "', email='" . $email . "', organisation='" . $organisation . "', contact_number='" . $contact_number . "' WHERE user_id='" . $_SESSION['user_id'] . "'";
+        // $query = "UPDATE users SET firstname='" . $firstname. "', lastname='" . $lastname . "', email='" . $email . "', organisation='" . $organisation . "', contact_number='" . $contact_number . "' WHERE user_id='" . $_SESSION['user_id'] . "'";
+        $query = "UPDATE users SET firstname='jeff' WHERE user_id=" . $_SESSION['user_id'] . "'";
         
         //check to see if the insert was successful
         if ($connection->query($query) === TRUE) {
