00efc444a8136fb227f2a783e5871b2b0a902f67
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-02-07 20:34:37 +0000

    added try catch for debugging

--- scripts/insert_new_user.php
+++ scripts/insert_new_user.php
@@ -21,13 +21,18 @@
 
         // query database and insert the new announcement into the announcements table
         $sql = "INSERT INTO users (firstname, lastname, email, contact_number, organisation, account_type) VALUES (:firstname, :lastname, :email, :contact_number, :organisation, :account_type);";
+        // INSERT INTO users (firstname, lastname, email, contact_number, organisation, account_type) VALUES ('adam', 'ldf', 'email', '1232', 'ardasf', 'practitioner')
         $stmt = $connectionPDO->prepare($sql);
         
         //check to see if the insert was successful
-        if ($stmt->execute(['firstname' => $firstname, 'lastname' => $lastname, 'email' => $email, 'contact_number' => $contact_number, 'organisation' => $organisation, 'account_type' => $account_type])) {
-            exit('*user_added_successfully*');
-        } else {
-            exit('Error: ' . $connectionPDO->error);
+        try{
+            if ($stmt->execute(['firstname' => $firstname, 'lastname' => $lastname, 'email' => $email, 'contact_number' => $contact_number, 'organisation' => $organisation, 'account_type' => $account_type])) {
+                exit('*user_added_successfully*');
+            } else {
+                exit('Error: ' . $connectionPDO->error);
+            }
+        } catch(Exception) {
+            exit('error');
         }
 
         $stmt = null;
