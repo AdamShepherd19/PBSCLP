c13d37550569ee73c0904f3dbae97295ac1ad217
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-02-07 20:44:52 +0000

    moved back button to outside if statement

--- pages/add_new_user.php
+++ pages/add_new_user.php
@@ -141,15 +141,16 @@
 
                                     $('.main-content').html(successHTML);
 
-                                    // onclick function for new button to return to landing page
-                                    $("#return").on('click', function(){
-                                        window.location.replace('manage_users.php');
-                                    });
-
                                 } else {
                                     //display error message if the php could not be executed
-                                    $('.main-content').html("<h3> There was an error processing your request. Please try again </h3><br>Error" + response);
+                                    $('.main-content').html("<h3> There was an error processing your request. Please try again </h3><br>Error" + response +
+                                        "<br><input type='button' id='return' class='pbs-button pbs-button-green' value='Confirm'>");
                                 }
+
+                                // onclick function for new button to return to landing page
+                                $("#return").on('click', function(){
+                                    window.location.replace('manage_users.php');
+                                });
                             },
                             datatype: 'text'
                         });
