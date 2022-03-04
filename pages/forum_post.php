33e33f02d6265b9e5a7e0ccca12541eddfff8b17
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-03-04 12:47:30 +0000

    trying without session variables

--- pages/forum_post.php
+++ pages/forum_post.php
@@ -157,8 +157,8 @@
                                     var new_comment = '<div class="card">' +
                                         '<div class="card-body post-comment">' +
                                             '<p class="comment-text">' + comment + '</p>' +
-                                            '<span class="comment-subtext"><i>' + <?php echo $_SESSION['firstname']; ?>  + ' ' + 
-                                            <?php echo $_SESSION['lastname']; ?> + ' - ' + 'date' + '</i></span>' +
+                                            '<span class="comment-subtext"><i>' + 'firstname' + ' ' + 
+                                            'lastname' + ' - ' + 'date' + '</i></span>' +
                                         '</div></div><br>';
 
                                     $('#comment-section').append(new_comment);
