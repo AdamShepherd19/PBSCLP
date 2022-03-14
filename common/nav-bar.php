ed4425e7fec01eb379c39916cb917905d2d3f578
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-03-14 17:46:36 +0000

    added nav-link class

--- common/nav-bar.php
+++ common/nav-bar.php
@@ -34,12 +34,12 @@
                 </li>
 
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle nav-link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         More
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="key_people.php">Key People</a>
-                        <a class="dropdown-item" href="faq.php">FAQ</a>
+                        <a class="dropdown-item nav-link" href="key_people.php">Key People</a>
+                        <a class="dropdown-item nav-link" href="faq.php">FAQ</a>
                     <!-- <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="#">Something else here</a>
                     </div> -->
