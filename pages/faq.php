a4202e0b9e4385853550f323ff2d634104365524
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-03-14 20:06:26 +0000

    added id to faq wrapper

--- pages/faq.php
+++ pages/faq.php
@@ -51,16 +51,16 @@
                 <input type="button" id="new-faq-button" class="pbs-button pbs-button-green" value="New FAQ">
             </div>
 
-            <div class="faq-wrapper">
+            <div class="faq-wrapper" id="#faq-wrapper">
 
-                <div class="card">
+                <!-- <div class="card">
                     <div class="card-header">
                         <h3>Q - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam?</h3>
                     </div>
                     <div class="card-body">
                         <p>A - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
-                </div>
+                </div> -->
                 
             </div>
         </div>
