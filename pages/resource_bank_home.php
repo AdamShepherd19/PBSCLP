3eec2c55f297311b4945757fcdc16aa418a04964
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-03-23 12:04:53 +0000

    added manage content button

--- pages/resource_bank_home.php
+++ pages/resource_bank_home.php
@@ -47,6 +47,9 @@
         </div>
 
         <div class="main-content">
+            <div class="search-wrapper">
+                <input type="button" id="manage-content-button" class="pbs-button pbs-button-green" value="Manage Content">
+            </div>
 
             <div class="inner-wrapper">
 
