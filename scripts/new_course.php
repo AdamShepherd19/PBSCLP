6fd29af885ac4c13844550c373b31183216ec940
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-03-23 12:53:54 +0000

    updated isset check

--- scripts/new_course.php
+++ scripts/new_course.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     
-    if(isset($_POST['commentPHP'])) {
+    if(isset($_POST['course_namePHP'])) {
         
         $pass = file_get_contents('../../pass.txt', true);
 
