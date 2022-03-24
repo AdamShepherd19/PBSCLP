4f0f9db27edab8a49a8ecb4ae508b123f4fce255
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-03-24 20:23:58 +0000

    testing without if

--- pages/admin_edit_profile.php
+++ pages/admin_edit_profile.php
@@ -146,12 +146,13 @@
                                 $("#courses").html("");
                                 for(var i = 0; i < response.length; i++) {
                                     let output;
-                                    
-                                    if (list_of_assigned_course_id.includes(response[i].course_id)) {
-                                        let output = '<input type="checkbox" id="edit-cid-' + response[i].course_id + '" class="pbs-form-check-box" value="' + response[i].course_name + '" checked><label for="edit-cid-' + response[i].course_id + '">' + response[i].course_name + '</label><br>';
-                                    } else {
-                                        let output = '<input type="checkbox" id="edit-cid-' + response[i].course_id + '" class="pbs-form-check-box" value="' + response[i].course_name + '"><label for="edit-cid-' + response[i].course_id + '">' + response[i].course_name + '</label><br>';
-                                    }
+
+                                    // if (list_of_assigned_course_id.includes(response[i].course_id)) {
+                                    //     let output = '<input type="checkbox" id="edit-cid-' + response[i].course_id + '" class="pbs-form-check-box" value="' + response[i].course_name + '" checked><label for="edit-cid-' + response[i].course_id + '">' + response[i].course_name + '</label><br>';
+                                    // } else {
+                                    //     let output = '<input type="checkbox" id="edit-cid-' + response[i].course_id + '" class="pbs-form-check-box" value="' + response[i].course_name + '"><label for="edit-cid-' + response[i].course_id + '">' + response[i].course_name + '</label><br>';
+                                    // }
+                                    let output = '<input type="checkbox" id="edit-cid-' + response[i].course_id + '" class="pbs-form-check-box" value="' + response[i].course_name + '"><label for="edit-cid-' + response[i].course_id + '">' + response[i].course_name + '</label><br>';
                                     
                                     $("#courses").append(output);
                                 }
