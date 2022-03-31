63102dd8589967be11e6330d41ae35c17ed6ae6d
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-03-31 12:59:05 +0100

    moved video to main content div

--- pages/video.php
+++ pages/video.php
@@ -50,11 +50,9 @@
         </div>
 
         <div class="main-content">
-            <div class="inner-wrapper">
             <!-- <iframe width="853" height="480" src="https://www.youtube.com/embed/_-wmIW6FSVo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
-                <iframe src="https://www.youtube.com/embed/_-wmIW6FSVo" class="pbs-video"> </iframe>
+            <iframe src="https://www.youtube.com/embed/_-wmIW6FSVo" class="pbs-video"> </iframe>
 
-            </div>
         </div>
 
         
