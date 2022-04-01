7d7463b6453e44f8b88c4f8c337894b59ed4c104
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-04-01 13:00:53 +0100

    added correct query to prepare

--- scripts/insert_new_user.php
+++ scripts/insert_new_user.php
@@ -44,7 +44,7 @@
                     }
                 }
 
-                $stmt = $connectionPDO->prepare($sql);
+                $stmt = $connectionPDO->prepare($query);
                 if ($stmt->execute()) {
                     exit('*user_added_successfully*');
                 } else {
