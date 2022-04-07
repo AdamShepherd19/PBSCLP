efbf43438656115fc4f5b9e2487487e91aecfb0b
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-04-07 15:06:58 +0100

    added amend posts page

---
+++ pages/amend_posts.php
@@ -0,0 +1,115 @@
+<?php
+    session_start();
+
+    if(!isset($_SESSION['logged_in'])){
+        header('Location: https://pbsclp.info');
+        exit();
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+
+        <!-- bootstrap metadata -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+        <!-- Bootstrap javascript include links -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+        <!-- include jQuery -->
+        <script src="../includes/jquery.js"></script>
+
+        <link rel="stylesheet" href="../stylesheets/style.css">
+        <link rel="stylesheet" href="../stylesheets/forum.css">
+
+        <title>Review Posts</title>
+        
+    </head>
+
+    <body>
+
+        <div id="pbs-nav-bar">
+            <?php
+                include "../common/nav-bar.php";
+            ?>
+        </div>
+
+        <div class="page-header">
+            <h1>Review Posts</h1>
+        </div>
+
+        <div class="main-content">
+            <div class="inner-wrapper">
+
+            </div>
+        </div>
+
+        
+        <script type="text/javascript">
+            $(document).ready(function () {
+
+                // only show administrator content if an admin logged in
+                var accountType = '<?php echo $_SESSION['account_type']; ?>';
+                if (accountType != 'administrator') {
+                    $('.admin-only').hide();
+                } else {
+                    $('.admin-only').show();
+                }
+
+                $.ajax({
+                    url: '../scripts/get_forum_posts.php',
+                    type: 'get',
+                    dataType: 'JSON',
+                    data: {
+                        approvedPHP: '0',
+                        feedback_providedPHP: '1',
+                        review_posts: '1'
+                    },
+                    success: function(response) {
+                        if (response.includes("*warning_no_posts_found*")) {
+                            var message = "<div class='card'><h4 class='card-header'> There are no posts that need reviewed!</div>"
+
+                            $(".forum-wrapper").append(message);
+                        } else {
+                            for(var x = 0; x < response.length; x++) {
+                                var message = '<div class="forum-post no-pointer-change card" id="thread-id-' + response[x].thread_id + '">' +
+                                    '<div class="card-header">' + response[x].title + '<br><span><i> - ' + response[x].firstname + ' ' + response[x].lastname + '</i></span>' + '</div>' +
+                                    '<div class="card-body">' +
+                                        '<p>' + response[x].content + '</p>' +
+                                        // '<input type="button" id="review-post-' + response[x].thread_id + '" class="pbs-button pbs-button-orange review-button" value="Review">' +
+                                        // '<input type="button" id="approve-post-' + response[x].thread_id + '" class="pbs-button pbs-button-green approve-button" value="Approve">' +
+                                    '</div></div><br>';
+
+                                $(".forum-wrapper").append(message);
+                            }
+
+                            // $(document).on("click", ".approve-button" , function() {
+                            //     var contentPanelId = jQuery(this).attr("id");
+                            //     var thread_id = contentPanelId.split(/[-]+/).pop();
+
+                            //     window.location.replace('approve_forum_post.php?threadId=' + thread_id);
+                            // });
+
+                            // $(document).on("click", ".review-button" , function() {
+                            //     var contentPanelId = jQuery(this).attr("id");
+                            //     var thread_id = contentPanelId.split(/[-]+/).pop();
+
+                            //     window.location.replace('review_individual_post.php?threadId=' + thread_id);
+                            // });
+                        }
+
+                    }
+                });
+            });
+        </script>
+        
+    </body>
+</html>
--- scripts/get_forum_posts.php
+++ scripts/get_forum_posts.php
@@ -13,7 +13,19 @@
         exit('*database_connection_error*');
     }
 
-    if(isset($_GET['approvedPHP']) && isset($_GET['feedback_providedPHP'])) {
+    if (isset($_GET['approvedPHP']) && isset($_GET['feedback_providedPHP']) && isset($_GET['review_posts'])) {
+
+        $approved = $_GET['approvedPHP'];
+        $feedback_provided = $_GET['feedback_providedPHP'];
+        $user_id = $_SESSION['user_id'];
+
+        $sql = "SELECT * FROM threads WHERE approved=:approved AND feedback_provided=:feedback_provided AND user_id=:user_id ORDER BY post_time DESC";
+
+        $stmt = $connectionPDO->prepare($sql);
+        $stmt->execute(['approved' => $approved, 'feedback_provided' => $feedback_provided, 'user_id' = $user_id]);
+        $result = $stmt->fetchAll();
+
+    } else if(isset($_GET['approvedPHP']) && isset($_GET['feedback_providedPHP'])) {
         
         $approved = $_GET['approvedPHP'];
         $feedback_provided = $_GET['feedback_providedPHP'];
@@ -25,7 +37,7 @@
         $result = $stmt->fetchAll();
 
     } else {
-        
+
         if (isset($_GET['approvedPHP'])) {
             $approved = $_GET['approvedPHP'];
             $sql = "SELECT * FROM threads WHERE approved=? ORDER BY post_time DESC";
