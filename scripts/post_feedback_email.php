837da9e4c6189d0fd549a98f566b9159b3cd9304
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-04-07 20:08:46 +0100

    created script to send email alerting of reviewal

---
+++ scripts/post_feedback_email.php
@@ -0,0 +1,55 @@
+<?php
+    // Reference Links:
+    // https://laratutorials.com/php-send-reset-password-link-email/
+    // https://github.com/PHPMailer/PHPMailer
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+    
+
+    require '../includes/PHPMailer/src/Exception.php';
+    require '../includes/PHPMailer/src/PHPMailer.php';
+    require '../includes/PHPMailer/src/SMTP.php';
+
+
+    function sendEmail($email_to, $name_to, $post_title, $post_content, $post_feedback) {
+        $e_pass = file_get_contents('../../f-e-pass.txt', true);
+
+        $mail = new PHPMailer();
+        $mail->CharSet =  "utf-8";
+        $mail->IsSMTP();
+        
+        $mail->SMTPAuth = true; // enable SMTP authentication
+
+        $mail->Username = "forumposts@pbsclp.info";
+        $mail->Password = $e_pass;
+
+        $mail->SMTPSecure = "ssl";
+        
+        $mail->Host = "mail.pbsclp.info"; // sets pbsclp mail server as the SMTP server
+        $mail->Port = "465"; // set the SMTP port for the pbsclp server
+
+        $mail->From='forumposts@pbsclp.info';
+        $mail->FromName='PBSCLP Forum Post System';
+
+        $mail->AddAddress($email_to, $name_to);
+        $mail->Subject  =  'Forum Post Feedback';
+        $mail->IsHTML(true);
+
+        $mail->Body    = '<h1> Forum Post Feedback </h1> <br> A forum post you submitted has received some feedback. Please log into the PBSCLP platform to review the post. <br><br> <h4> Post Title </h4>' . $post_title . '<br><br> <h4> Post Content </h4>' . $post_content . '<br><br> <h4> Post Feedback </h4>' . $post_feedback;
+
+        $mail->AltBody = 'New Forum Post. A new post has been submitted for approval. Please log in to the PBSCLP platform to review the new post. Post Title: ' . $post_title . 'Post Content: ' . $post_content . 'Post Feedback: ' . $post_feedback;
+
+        if($mail->Send())
+        {
+            return "*email_sent_successfully*";
+        }
+        else
+        {
+            $msg = "Mail Error - >" . $mail->ErrorInfo;
+            return $msg;
+        }
+    }
+
+?>
