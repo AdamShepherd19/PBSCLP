0190002c85f048dfe0982648a0102ee27ecad4ce
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-04-08 16:04:31 +0100

    moved position of button

--- pages/landing.php
+++ pages/landing.php
@@ -65,8 +65,8 @@
 
             <div class="landing-nav-box-wrapper">
                 <input type="button" id="review-forum-posts" class="pbs-button pbs-button-orange admin-only" value="New Forum Posts"> <br/>
-                <input type="button" id="new-announcement" class="pbs-button pbs-button-blue admin-only" value="New Announcement"> <br/>
                 <input type="button" id="amend-posts" class="pbs-button pbs-button-orange" value="Amend Posts"> <br/>
+                <input type="button" id="new-announcement" class="pbs-button pbs-button-blue admin-only" value="New Announcement"> <br/>
                 <input type="button" id="forum" class="pbs-button pbs-button-white" value="Forum"> <br />
                 <input type="button" id="resource-bank" class="pbs-button pbs-button-blue" value="Resource Bank"> <br />
                 <input type="button" id="profile" class="pbs-button pbs-button-grey" value="Profile"> <br />
