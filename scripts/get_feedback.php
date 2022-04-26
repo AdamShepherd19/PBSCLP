77caaee292b8aceefc9a612cc9b6e7c7868735c4
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-04-26 16:04:52 +0100

    commented get files

--- scripts/get_feedback.php
+++ scripts/get_feedback.php
@@ -1,3 +1,16 @@
+<!--
+    ============================================
+        - PBSCLP | get_feedback
+        - Adam Shepherd
+        - PBSCLP
+        - April 2022
+
+        This script queries and returns the
+        feedback provided by an admin for a 
+        specific forum post
+    ============================================
+-->
+
 <?php
     if(isset($_GET['threadIDPHP'])){
         $thread_id = $_GET['threadIDPHP'];
