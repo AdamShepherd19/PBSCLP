4b46a067df1b7219634e4e720ccc3eed846b391c
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-04-26 15:30:38 +0100

    commented forum page

--- pages/forum.php
+++ pages/forum.php
@@ -1,3 +1,19 @@
+<!--
+    ============================================
+        - PBSCLP | forum
+        - Adam Shepherd
+        - PBSCLP
+        - April 2022
+
+        This file contains the page for a
+        practitioner to view the forum system.
+        They will be presented with a list of all
+        approved forum posts and they can select
+        one to view the individual page for it
+        and view the comments
+    ============================================
+-->
+
 <?php
     session_start();
 
