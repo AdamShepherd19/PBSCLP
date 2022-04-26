b0d756136a1bede18e38702f8266a8de71a080b0
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-04-26 16:09:55 +0100

    commented logout

--- scripts/logout.php
+++ scripts/logout.php
@@ -1,3 +1,16 @@
+<!--
+    ============================================
+        - PBSCLP | logout
+        - Adam Shepherd
+        - PBSCLP
+        - April 2022
+
+        This script unsets and destroys all 
+        session variables, loggin the user out
+        and taking them back to the login page
+    ============================================
+-->
+
 <?php
     session_start();
 
