f1d16eb7777a94c742f6099f2d2dabcf07196695
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2022-06-30 11:27:43 +0100

    testing new api key

--- scripts/sendinblue/register_account.php
+++ scripts/sendinblue/register_account.php
@@ -1,13 +1,13 @@
 <?php
     require_once('../../includes/vendor/autoload.php');
 
-    $credentials = SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKey('pbsclp', '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');
+    $credentials = SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKey('api-key', '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');
     $apiInstance = new SendinBlue\Client\Api\TransactionalEmailsApi(new GuzzleHttp\Client(),$credentials);
 
     $sendSmtpEmail = new \SendinBlue\Client\Model\SendSmtpEmail([
         'subject' => 'from the PHP SDK!',
-        'sender' => ['name' => 'PBSclp Account Setup', 'email' => 'passwordreset@pbsclp.info'],
-        'replyTo' => ['name' => 'PBSclp Account Setup', 'email' => 'passwordreset@pbsclp.info'],
+        'sender' => ['name' => 'Sendinblue', 'email' => 'contact@sendinblue.com'],
+        'replyTo' => ['name' => 'Sendinblue', 'email' => 'contact@sendinblue.com'],
         'to' => [[ 'name' => 'Adam', 'email' => 'shepherd.adam.2000@gmail.com']],
         'htmlContent' => '<html><body><h1>This is a transactional email {{params.bodyMessage}}</h1></body></html>',
         'params' => ['bodyMessage' => 'made just for you! (sendinblue api test)']
