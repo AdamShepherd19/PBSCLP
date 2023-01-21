7b444d29a25b01dada686e1a9ae48e350ba17dea
AdamShepherd19 <shepherd.adam.2000@gmail.com>
2023-01-21 14:49:29 +0000

    Added delete announcement functionality

--- pages/new_announcement.php
+++ pages/new_announcement.php
@@ -23,62 +23,75 @@ if ($_SESSION['account_type'] != 'administrator') {
 }
 
 $pass = file_get_contents('../../pass.txt', true);
-
-if (isset($_POST['titlePHP'])) {
-    //connect to database
-    try {
-        $connectionPDO = new PDO('mysql:host=localhost;dbname=pbsclp_pbsclp', 'pbsclp', $pass);
-        $connectionPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        exit('*database_connection_error*');
-    }
-
-    //retrieve title, content and author for the new post
-    $title = $_POST['titlePHP'];
-    $content = $_POST['contentPHP'];
-    $link = $_POST['linkPHP'];
-    $user_id = $_SESSION['user_id'];
-
-    try {
-        // query database and insert the new announcement into the announcements table
-        if (isset($_POST['announcement_idPHP']) && $_POST['announcement_idPHP'] != -1) {
-            $sql = "UPDATE announcements SET title=:title, content=:content, user_id=:user_id, link=:link WHERE announcement_id=:announcement_id;";
-            $stmt = $connectionPDO->prepare($sql);
-
-            //check to see if the insert was successful
-            if ($stmt->execute(['title' => $title, 'content' => $content, 'user_id' => $user_id, 'link' => $link, 'announcement_id' => $_POST['announcement_idPHP']])) {
-                exit('*announcement_updated_successfully*');
-            } else {
-                exit('Error: ' . $connectionPDO->error);
-            }
-        } else {
-            $sql = "INSERT INTO announcements (title, content, user_id, link) VALUES (:title, :content, :user_id, :link);";
-            $stmt = $connectionPDO->prepare($sql);
-
-            //check to see if the insert was successful
-            if ($stmt->execute(['title' => $title, 'content' => $content, 'user_id' => $user_id, 'link' => $link])) {
-                exit('*announcement_created_successfully*');
-            } else {
-                exit('Error: ' . $connectionPDO->error);
-            }
-        }
-    } catch(PDOException $e) {
-        exit('*query_error* - ' . $e);
-    }
-
-    // $stmt = $connectionPDO->prepare($sql);
-
-    // //check to see if the insert was successful
-    // if ($stmt->execute(['title' => $title, 'content' => $content, 'user_id' => $user_id, 'link' => $link])) {
-    //     exit('success');
-    // } else {
-    //     exit('Error: ' . $connectionPDO->error);
-    // }
-
-    $stmt = null;
-    $connectionPDO = null;
-
+//connect to database
+try {
+    $connectionPDO = new PDO('mysql:host=localhost;dbname=pbsclp_pbsclp', 'pbsclp', $pass);
+    $connectionPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    exit('*database_connection_error*');
 }
+
+if (isset($_POST['delete_announcementPHP']) && $_POST['delete_announcementPHP'] == 1) {
+    $sql = "DELETE FROM announcements WHERE announcement_id=:announcement_id";
+    $stmt = $connectionPDO->prepare($sql);
+
+    //check to see if the insert was successful
+    if ($stmt->execute(['announcement_id' => $_POST['announcement_idPHP']])) {
+        exit('*announcement_deleted_successfully*');
+    } else {
+        exit('Error: ' . $connectionPDO->error);
+    }
+} else {
+    if (isset($_POST['titlePHP'])) {
+        //retrieve title, content and author for the new post
+        $title = $_POST['titlePHP'];
+        $content = $_POST['contentPHP'];
+        $link = $_POST['linkPHP'];
+        $user_id = $_SESSION['user_id'];
+
+        try {
+            // query database and insert the new announcement into the announcements table
+            if (isset($_POST['announcement_idPHP']) && $_POST['announcement_idPHP'] != -1) {
+                $sql = "UPDATE announcements SET title=:title, content=:content, user_id=:user_id, link=:link WHERE announcement_id=:announcement_id;";
+                $stmt = $connectionPDO->prepare($sql);
+
+                //check to see if the insert was successful
+                if ($stmt->execute(['title' => $title, 'content' => $content, 'user_id' => $user_id, 'link' => $link, 'announcement_id' => $_POST['announcement_idPHP']])) {
+                    exit('*announcement_updated_successfully*');
+                } else {
+                    exit('Error: ' . $connectionPDO->error);
+                }
+            } else {
+                $sql = "INSERT INTO announcements (title, content, user_id, link) VALUES (:title, :content, :user_id, :link);";
+                $stmt = $connectionPDO->prepare($sql);
+
+                //check to see if the insert was successful
+                if ($stmt->execute(['title' => $title, 'content' => $content, 'user_id' => $user_id, 'link' => $link])) {
+                    exit('*announcement_created_successfully*');
+                } else {
+                    exit('Error: ' . $connectionPDO->error);
+                }
+            }
+        } catch (PDOException $e) {
+            exit('*query_error* - ' . $e);
+        }
+
+        // $stmt = $connectionPDO->prepare($sql);
+
+        // //check to see if the insert was successful
+        // if ($stmt->execute(['title' => $title, 'content' => $content, 'user_id' => $user_id, 'link' => $link])) {
+        //     exit('success');
+        // } else {
+        //     exit('Error: ' . $connectionPDO->error);
+        // }
+
+
+
+    }
+}
+
+$stmt = null;
+$connectionPDO = null;
 
 ?>
 
@@ -142,7 +155,8 @@ if (isset($_POST['titlePHP'])) {
                 <input type="text" id="link" class="pbs-form-text-box" placeholder="Enter optional link..."><br /><br />
 
                 <div class="button-wrapper">
-                    <input type="button" id="announcement-cancel" class="pbs-button pbs-button-red" value="Cancel">
+                    <input type="button" id="announcement-cancel" class="pbs-button pbs-button-yellow" value="Cancel">
+                    <input type="button" id="announcement-delete" class="pbs-button pbs-button-red" value="Delete">
                     <input type="button" id="announcement-post" class="pbs-button pbs-button-green" value="Post">
                 </div>
             </form>
@@ -154,10 +168,10 @@ if (isset($_POST['titlePHP'])) {
     <script type="text/javascript">
         $(document).ready(function () {
 
-            var announcement_id = "<?php Print($_GET['aid']); ?>";
+            var announcement_id = "<?php print($_GET['aid']); ?>";
             var title, content, link;
 
-            if(announcement_id){
+            if (announcement_id) {
                 $("#page-title").html("Edit Announcement");
                 console.log(announcement_id);
 
@@ -187,6 +201,34 @@ if (isset($_POST['titlePHP'])) {
                 window.location.replace('landing.php');
             });
 
+            //onclick function for the delete button
+            $("#announcement-delete").on('click', function () {
+                $.ajax({
+                    method: 'POST',
+                    url: "new_announcement.php",
+                    data: {
+                        announcement_idPHP: announcement_id,
+                        delete_announcementPHP: 1
+                    },
+                    success: function (response) {
+                        console.log(response);
+                        if (response.includes("*announcement_deleted_successfully*")) {
+                            var successHTML = "<h3>Your post has been deleted succesfully. Please click the button below to return to the landing page.</h3><br> " +
+                                "<input type='button' id='return' class='pbs-button pbs-button-green' value='Confirm'>";
+
+                            $('.main-content').html(successHTML);
+
+                            // onclick function for new button to return to landing page
+                            $("#return").on('click', function () {
+                                window.location.replace('landing.php');
+                            });
+                        } else {
+                            alert("Deletion failed. Please try again.");
+                        }
+                    }
+                });
+            });
+
             // onclick function for the post announcement button
             $("#announcement-post").on('click', function () {
                 //retrieve data from form
@@ -196,7 +238,7 @@ if (isset($_POST['titlePHP'])) {
 
                 const isEmpty = str => !str.trim().length;
 
-                if(!isEmpty($('#link').val())){
+                if (!isEmpty($('#link').val())) {
                     link = $('#link').val();
                     let url_regex = /https?:\/\//;
                     if (url_regex.test(String(link).toLowerCase()) == false) {
